5c09d0c729ce3de483693cd696d45857528b2c3d
nouisaigp <noeisaigp98@gmail.com>
2022-05-23 21:06:16 -0500

    view Page con fragments agregado

--- app/src/main/java/com/example/carspeak/MainActivity.kt
+++ app/src/main/java/com/example/carspeak/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.carspeak
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
+import com.example.carspeak.util.ViewPagerAdapter
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val viewPager: ViewPager2 = findViewById(R.id.viewPager)
+
+        val fragments: ArrayList<Fragment> = arrayListOf(
+            PrincipalFragment(),
+            BotonesFragment()
+        )
+
+        val adapter = ViewPagerAdapter(fragments,this)
+        viewPager.adapter = adapter
     }
+
 }
